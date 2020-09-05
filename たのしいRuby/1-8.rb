f6678ef048b88e1021bdb3781f2e4da7a60d6a57
Naoki Maruyama <m.naoki85@gmail.com>
2020-09-06 00:04:44 +0900

    [Finish]1-8.rb

---
+++ たのしいRuby/1-8.rb
@@ -0,0 +1,21 @@
+names = [" maruyama","maruyama","maruyama","maruyama"]
+
+p names.size
+
+if names[2] == "maruyama"
+    puts "maruyamaです"
+elsif names[3] == "丸山です。"
+    puts "maruyamaです。"
+elsif namse[1] == "maruyuamadesu."
+
+end
+
+
+names.each_index do |n|
+    p n
+
+end
+
+names.each do |n|
+    p n
+end
