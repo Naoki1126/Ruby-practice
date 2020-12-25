4db82b3d88ff084ac137c7deeb40c1b916b2f49e
Naoki Maruyama <m.naoki85@gmail.com>
2020-12-25 22:03:11 +0900

    add

--- hex_oct.rb
+++ hex_oct.rb
@@ -12,3 +12,22 @@ p a[0,3].map{ |a| a }
 p a[0..-2]
 p a[0..-1]
 p a[0...-2]
+
+p 07
+p 010
+p 011
+p 012
+
+(1..10).each do |i|
+    p i
+end
+
+(1...10).to_a.reverse_each do |i|
+    p i
+end
+
+p (1...10).to_a
+
+
+er = StandardError.new
+p er.class
